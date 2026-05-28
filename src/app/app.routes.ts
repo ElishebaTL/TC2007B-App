18eac42efc136556a0c539af4c4127b7ebb2d26d
ElishebaTL <A01736967@tec.mx>
2026-05-27 19:28:48 -0600

    initial app structure and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,36 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'login',
+    loadComponent: () => import('./auth/pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./parent/pages/dashboard/dashboard.page').then( m => m.DashboardPage)
+  },
+  {
+    path: 'report-cards',
+    loadComponent: () => import('./parent/pages/report-cards/report-cards.page').then( m => m.ReportCardsPage)
+  },
+  {
+    path: 'report-card-detail',
+    loadComponent: () => import('./parent/pages/report-card-detail/report-card-detail.page').then( m => m.ReportCardDetailPage)
+  },
+  {
+    path: 'comments',
+    loadComponent: () => import('./parent/pages/comments/comments.page').then( m => m.CommentsPage)
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./parent/pages/profile/profile.page').then( m => m.ProfilePage)
   },
 ];
